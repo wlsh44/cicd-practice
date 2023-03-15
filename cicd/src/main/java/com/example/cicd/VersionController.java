4cc188472d44439bd60178cff201b9feb8d0b595
wlsh44 <k87913j@gmail.com>
2023-03-15 22:41:31 +0900

    chore: 도커 파일 세팅

--- cicd/src/main/java/com/example/cicd/VersionController.java
+++ cicd/src/main/java/com/example/cicd/VersionController.java
@@ -1,5 +1,6 @@
 package com.example.cicd;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionController {
 
+    @Value("${application-env}")
+    private String env;
+
     @GetMapping("/")
     public ResponseEntity<String> version() {
         String version = "1.0.0";
-        return ResponseEntity.ok(version);
+        return ResponseEntity.ok(env + " " + version);
     }
 }
