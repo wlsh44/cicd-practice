ca79e1906c86f76ceaa7adcad26a0966c02f9c92
wlsh44 <k87913j@gmail.com>
2023-03-18 20:52:30 +0900

    chore: 배포 스크립트 추가

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+# !/bin/bash
+
+DEFAULT_CONF="/root/nginx/default.conf"
+IS_BLUE_RUNNING=$(docker ps | grep blue)
+
+if [ -n "$IS_BLUE_RUNNING" ]; then
+    DEPLOY="green"
+    RUNNING="blue"
+else
+    DEPLOY="blue"
+    RUNNING="green"
+fi
+
+echo "########################################################$RUNNING is running...########################################################"
+
+echo "########################################################Pull docker image to $DEPLOY...########################################################"
+docker compose pull $DEPLOY
+
+echo "########################################################Deploy $DEPLOY...########################################################"
+docker compose up -d $DEPLOY
+
+while true
+do
+    echo "########################################################health checking $DEPLOY...########################################################"
+    REQUEST=$(docker exec nginx curl http://$DEPLOY:8080)
+    echo $REQUEST
+    if [ -n "$REQUEST" ]; then
+        break;
+    fi
+    sleep 3
+done;
+
+echo "########################################################change nginx host $RUNNING to $DEPLOY...########################################################"
+sed -i "s/$RUNNING/$DEPLOY/g" $DEFAULT_CONF
+
+echo "########################################################nginx reload...########################################################"
+docker exec -d nginx service nginx reload
+
+echo "########################################################stop $RUNNING...########################################################"
+docker compose stop $RUNNING
+
+echo "########################################################deploy finished!!########################################################"
